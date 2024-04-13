8263fab91c89d622def8aa1d49dc8f57b1067c98
loubnakamalrady <loubnakamal1998@hotmail.com>
2024-04-13 23:57:42 +0200

    added pages, and components, creation of header component with responsiveness

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/HomePage/Home";
+import SignIn from "./Pages/SignIn/Sign-in";
+import SignOut from "./Pages/SignOut/Sign-out";
+import SignUp from "./Pages/SignUp/Sign-up";
+import Profile from "./Pages/ProfilePage/Profile";
+import About from "./Pages/AboutPage/About";
+import Header from "./Components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,63 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import { FaSearch, FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-3">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">LMAO</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <div className="sm:flex hidden">
+          <ul className="flex gap-5 text-slate-700">
+            <Link to={"/"}>
+              <li className="hover:underline">Home</li>
+            </Link>
+            <Link to={"/about"}>
+              <li className="hover:underline">About</li>
+            </Link>
+            <Link to={"/sign-in"}>
+              <li className="hover:underline">Sign In</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="sm:hidden relative">
+          <FaBars
+            className="text-slate-600 cursor-pointer"
+            onClick={toggleMenu}
+          />
+          {showMenu && (
+            <ul className="absolute bg-white top-full right-1 mt-3 p-2 rounded shadow-md w-20">
+              <Link to={"/"}>
+                <li className="mb-1">Home</li>
+              </Link>
+              <Link to={"/about"}>
+                <li className="mb-1">About</li>
+              </Link>
+              <Link to={"/sign-in"}>
+                <li>Sign In</li>
+              </Link>
+            </ul>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
