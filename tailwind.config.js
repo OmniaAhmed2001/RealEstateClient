1563eab84d6a0dd5d05e99720fad9615a2d48a81
OmniaAhmed2001 <omn.ahmed2001@gmail.com>
2024-04-26 20:28:06 +0300

    feat: :sparkles: Create Responsive Home Page UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        buttons: '#FFB534',
+        hovers: '#FFCB74',
+        backgr: '#FDF5E8',
+      },
+      opacity: {
+        '40': '0.4',
+        '20': '0.2',
+      },
+    },
   },
   plugins: [],
 };
