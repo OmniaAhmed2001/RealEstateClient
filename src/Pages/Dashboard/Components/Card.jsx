510c76524a2997dc7a240b26677972e4f2a5b60c
loubnakamalrady <loubnakamal1998@hotmail.com>
2024-04-28 20:12:36 +0300

    Update Dashboard UI

--- src/Pages/Dashboard/Components/Card.jsx
+++ src/Pages/Dashboard/Components/Card.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
 
-export default function Card() {
+export default function Card(icon, description, value) {
+  async function GetAllEstates() {
+    const data = await fetch("");
+  }
+
   return (
-    <div>Card</div>
-  )
+    <>
+      <div className="flex">
+        <div className="flex flex-col justify-center items-center bg-fefbf6 w-72 h-80 m-28 relative rounded-2xl shadow-xl">
+          <div className="bg-fefbf6 p-7 rounded-full absolute mb-6 bottom-56">
+            <img src="public/assets/image 9.png" />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-9">
+            <h1 className="text-3xl font-semibold">Total Real Estates</h1>
+            <p className="text-4xl font-bold text-ff9a62">2500</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center items-center bg-fefbf6 w-72 h-80 m-28 relative rounded-2xl shadow-xl">
+          <div className="bg-fefbf6 p-7 rounded-full absolute mb-6 bottom-56">
+            <img src="public/assets/image 9.png" />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-9">
+            <h1 className="text-3xl font-semibold">Total Real Estates</h1>
+            <p className="text-4xl font-bold text-ff9a62">2500</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center items-center bg-fefbf6 w-72 h-80 m-28 relative rounded-2xl shadow-xl">
+          <div className="bg-fefbf6 p-7 rounded-full absolute mb-6 bottom-56">
+            <img src="public/assets/image 9.png" />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-9">
+            <h1 className="text-3xl font-semibold">Total Real Estates</h1>
+            <p className="text-4xl font-bold text-ff9a62">2500</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,12 +3,12 @@ import React from "react";
 // eslint-disable-next-line no-unused-vars
 import BasicPie from "./Components/PieChart";
 import TickPlacementBars from "./Components/BarChart";
+import Card from "./Components/Card";
 
 export default function Dashboard() {
   return (
     <>
-      <BasicPie />
-      <TickPlacementBars />
+      <Card />
     </>
   );
 }
